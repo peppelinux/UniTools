2e490490209207a94e409832a648dbf979dc526b
Giuseppe De Marco <demarcog83@gmail.com>
2016-05-24 11:36:13 +0200

    Create mail_spool_cleaner.sh

---
+++ mail_spool_cleaner.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# automatic clean your mailbox
+
+# change it according to your path
+MAIL_SPOOL_FILE="/var/spool/mail/username"
+
+# percent
+EMAIL_PERCENT_TO_PURGE=33
+
+# count email
+EMAIL_COUNT=$(grep -c "^From: " $MAIL_SPOOL_FILE )
+
+#
+EMAIL_NUMBER_TO_PURGE=$(echo $(($EMAIL_COUNT * $EMAIL_PERCENT_TO_PURGE / 100)))
+
+# email purge
+mailx << EOF
+d 1-$EMAIL_NUMBER_TO_PURGE
+EOF
