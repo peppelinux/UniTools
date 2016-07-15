d90bb7e5ba4c2eb30b51b1a0115b6bcc99fa7b98
Giuseppe De Marco <demarcog83@gmail.com>
2016-07-15 13:00:11 +0200

    Update download_json_insegnamenti.py

--- schede_insegnamenti/download_json_insegnamenti.py
+++ schede_insegnamenti/download_json_insegnamenti.py
@@ -11,7 +11,6 @@ _regexp_grouped = r'([\t\s]+)?(?P<nome>(%s)+)\[(?P<codice>[0-9]+)\]( ?= ?)(?P<va
 _regexp         = r'([a-z]*)([\[0-9\]]*)( ?= ?)({.*});'
 _url            = 'http://www.unical.it/portale/portaltemplates/view/view_scheda_insegnamento.cfm'
 
-_file_to_save   = sys.argv[1]+'.%s'%datetime.datetime.now().strftime('%Y-%m-%d')
 
 def get_page(url):
     response = urllib2.urlopen(url)
@@ -61,6 +60,8 @@ def insegnamenti_dict(dati):
     return d
 
 if __name__ == '__main__':
+	_file_to_save   = sys.argv[1]+'.%s'%datetime.datetime.now().strftime('%Y-%m-%d')	
+	
     html = get_page(_url)
     dati = process_html(html, debug=False)
     #test(dati)
