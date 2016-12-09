21778e44895785db271b73de30e9d99f549f2fe1
Giuseppe De Marco <demarcog83@gmail.com>
2016-12-09 10:10:42 +0100

    ilo connection

---
+++ HP/ilo_tunnel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# once connected we can access ilo remote screen through firefox and java 1.8.
+# remember to whitelist the certificate
+
+SSHADDR='160.x.t.u'
+IPADDR='10.x.y.z'
+
+ssh root@$SSHADDR -L \*:2222:$IPADDR:22 -L \*:23:$IPADDR:23 -L \*:80:$IPADDR:80 -L \*:3389:$IPADDR:3389 -L \*:443:$IPADDR:443 -L \*:17988:$IPADDR:17988 -L \*:17990:$IPADDR:17990 -L \*:9300:$IPADDR:9300 -L \*:3002:$IPADDR:3002
