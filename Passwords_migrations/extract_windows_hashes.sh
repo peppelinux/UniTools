6428a93f6930467ef4212ac1a9e9685d0abbcbe4
Giuseppe De Marco <demarcog83@gmail.com>
2017-10-26 19:31:53 +0200

    Update extract_windows_hashes.sh

--- Passwords_migrations/extract_windows_hashes.sh
+++ Passwords_migrations/extract_windows_hashes.sh
@@ -13,7 +13,7 @@ Copy \\?\GLOBALROOT\Device\HarddiskVolumeShadowCopy1\Windows\System32\config\SYS
 Vssadmin delete shadows /For=C: /All
 
 # now copy ntds.dit and system files into your linux box :)
-apt install libesedb-utils 
+apt install libesedb-utils python-crypto
 
 ls 
 # ntds.dit  SAM  system
@@ -21,6 +21,10 @@ ls
 esedbexport ntds.dit 
 # it will create a subfolder called: ntds.dit.export
 cd ntds.dit.export
+
+git clone https://github.com/csababarta/ntdsxtract.git
+cd ntdsxtract && sudo python setup.py install
+
 dsusers.py datatable.3 link_table.4 ../_DSoutput2 --passwordhashes --lmoutfile LM.out --ntoutfile NT.out --pwdformat john --syshive ../system > domain.txt
 
 # everything with its NT hashes is right here
