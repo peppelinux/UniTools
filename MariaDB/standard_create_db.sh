48bd220297dc01beda98d9c5be1b28e4efd67611
Giuseppe De Marco <giuseppe.demarco@unical.it>
2017-12-17 00:00:19 +0100

    Update standard_create_db.sh

--- MariaDB/standard_create_db.sh
+++ MariaDB/standard_create_db.sh
@@ -5,5 +5,5 @@ export DB='slapd'
 
 mysql -u root -e "\
 CREATE USER ${USER}@'${HOST}' IDENTIFIED BY '${PASS}';\
-CREATE DATABASE ${DB};\
+CREATE DATABASE ${DB} CHARACTER SET utf8 COLLATE utf8_general_ci;\
 GRANT ALL PRIVILEGES ON ${DB}.* TO ${USER}@'${HOST}';"
