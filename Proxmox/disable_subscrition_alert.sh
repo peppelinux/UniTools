4711da39a4be75f54269efe70e2cf952245fb589
Giuseppe De Marco <giuseppe.demarco@unical.it>
2018-02-12 11:39:46 +0100

    Update disable_subscrition_alert.sh

--- Proxmox/disable_subscrition_alert.sh
+++ Proxmox/disable_subscrition_alert.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # tested on Proxmox 4.4
-
 cd /usr/share/pve-manager/ext6/
 cp pvemanagerlib.js pvemanagerlib.js.old
 sed  "s|if (data\.status !== 'Active') {|if (data\.status == 'Active') {|g" pvemanagerlib.js.old > pvemanagerlib.js
 
+# tested on Proxmox 5.0 and 5.1
 cd /usr/share/pve-manager/js/
 cp pvemanagerlib.js pvemanagerlib.js.old
 sed  "s|if (data\.status !== 'Active') {|if (data\.status == 'Active') {|g" pvemanagerlib.js.old > pvemanagerlib.js
