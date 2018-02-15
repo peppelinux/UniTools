9ac3618a4206ef4de5791e74a6c54f00768d8c9e
Giuseppe De Marco <giuseppe.demarco@unical.it>
2018-02-15 09:35:34 +0100

    Update check_nameserver_ttl.sh

--- check_nameserver_ttl.sh
+++ check_nameserver_ttl.sh
@@ -3,4 +3,4 @@ DNS_SERVER="160.97.7.13"
 MUL_FACTOR=0.0002777777777777778
 echo "Usage: this_command.sh host.domain.to.check"
 
-host -v $1 $DNS_SERVER | grep SOA | head -1 | awk -F' ' {'print $2'}  | awk '{printf "%4.3f\n",$0*'$MUL_FACTOR'}'
+host -v $1 $DNS_SERVER | grep SOA | head -1 | awk -F' ' {'print $2'}  | awk '{printf "Rinnovo ogni %4.2f ore\n",$0*'$MUL_FACTOR'}'
