64095ec4a1985b5eed1226358ca4c56dcfc1efca
Giuseppe De Marco <giuseppe.demarco@unical.it>
2018-02-17 21:35:40 +0100

    Update check_nameserver_ttl.sh

--- check_nameserver_ttl.sh
+++ check_nameserver_ttl.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+
 # MUL_FACTOR is sized on hours ((x / minutes /) hours)
 MUL_FACTOR=0.0002777777777777778
-echo "Usage: this_command.sh host.domain.to.check nameserver_to_query"
+
+if [ -z "$1" ] || [ -z "$2" ]
+  then
+        echo "I will check the ttl renewal of a domain on its soa dns"
+        echo "SOA record is a Start of Authority. Every domain must have a Start of Authority record at the cutover point where the domain is delegated from its parent domain"
+        echo "Usage: this_command.sh host.domain.to.check soanameserver.domain.to.check"
+        exit 1
+fi
 
 host -v $1 $2 | grep SOA | head -1 | awk -F' ' {'print $2'}  | awk '{printf "Rinnovo ogni %4.2f ore\n",$0*'$MUL_FACTOR'}'
+
