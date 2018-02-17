76b2d87ceb05a4dcffc1fb43bfeeab0c9814985a
Giuseppe De Marco <giuseppe.demarco@unical.it>
2018-02-17 21:35:58 +0100

    Update check_nameserver_ttl.sh

--- check_nameserver_ttl.sh
+++ check_nameserver_ttl.sh
@@ -5,7 +5,7 @@ MUL_FACTOR=0.0002777777777777778
 
 if [ -z "$1" ] || [ -z "$2" ]
   then
-        echo "I will check the ttl renewal of a domain on its soa dns"
+        echo "Check ttl renewal of a domain on its soa dns"
         echo "SOA record is a Start of Authority. Every domain must have a Start of Authority record at the cutover point where the domain is delegated from its parent domain"
         echo "Usage: this_command.sh host.domain.to.check soanameserver.domain.to.check"
         exit 1
