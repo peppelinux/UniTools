b2ea1079c2d1f418716fab63a68ced8445374e56
Giuseppe De Marco <giuseppe.demarco@unical.it>
2018-04-16 18:12:37 +0200

    Create infoweb_dialog_patch.js

---
+++ infoweb_dialog_patch.js
@@ -0,0 +1,38 @@
+if (!window.showModalDialog) {
+     window.showModalDialog = function (arg1, arg2, arg3) {
+
+        var w;
+        var h;
+        var resizable = "no";
+        var scroll = "no";
+        var status = "no";
+
+        // get the modal specs
+        var mdattrs = arg3.split(";");
+        for (i = 0; i < mdattrs.length; i++) {
+           var mdattr = mdattrs[i].split(":");
+
+           var n = mdattr[0];
+           var v = mdattr[1];
+           if (n) { n = n.trim().toLowerCase(); }
+           if (v) { v = v.trim().toLowerCase(); }
+
+           if (n == "dialogheight") {
+              h = v.replace("px", "");
+           } else if (n == "dialogwidth") {
+              w = v.replace("px", "");
+           } else if (n == "resizable") {
+              resizable = v;
+           } else if (n == "scroll") {
+              scroll = v;
+           } else if (n == "status") {
+              status = v;
+           }
+        }
+
+        var left = window.screenX + (window.outerWidth / 2) - (w / 2);
+        var top = window.screenY + (window.outerHeight / 2) - (h / 2);
+        var targetWin = window.open(arg1, arg1, 'toolbar=no, location=no, directories=no, status=' + status + ', menubar=no, scrollbars=' + scroll + ', resizable=' + resizable + ', copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+        targetWin.focus();
+     };
+  }
