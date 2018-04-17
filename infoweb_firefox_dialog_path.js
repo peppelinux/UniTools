efa22d33f648421229ad457e8c59b54b23f4b882
Giuseppe De Marco <giuseppe.demarco@unical.it>
2018-04-17 10:36:36 +0200

    Create infoweb_firefox_dialog_path.js

---
+++ infoweb_firefox_dialog_path.js
@@ -0,0 +1,74 @@
+(function () {
+window.spawn = window.spawn || function (gen) {
+    function continuer(verb, arg) {
+        var result;
+        try {
+            result = generator[verb](arg);
+        } catch (err) {
+            return Promise.reject(err);
+        }
+        if (result.done) {
+            return result.value;
+        } else {
+            return Promise.resolve(result.value).then(onFulfilled, onRejected);
+        }
+    }
+    var generator = gen();
+    var onFulfilled = continuer.bind(continuer, 'next');
+    var onRejected = continuer.bind(continuer, 'throw');
+    return onFulfilled();
+};
+window.showModalDialog = window.showModalDialog || function (url, arg, opt) {
+    url = url || '';
+    arg = arg || null;
+    opt = opt || 'dialogWidth:300px;dialogHeight:200px';
+    var caller = showModalDialog.caller.toString();
+    var dialog = document.body.appendChild(document.createElement('dialog'));
+    dialog.setAttribute('style', opt.replace(/dialog/gi, ''));
+    dialog.innerHTML = '<a href="#" id="dialog-close" style="position: absolute; top: 0; right: 4px; font-size: 20px; color: #000; text-decoration: none; outline: none;">&times;</a><iframe id="dialog-body" src="' + url + '" style="border: 0; width: 100%; height: 100%;"></iframe>';
+    document.getElementById('dialog-body').contentWindow.dialogArguments = arg;
+    document.getElementById('dialog-close').addEventListener('click', function (e) {
+        e.preventDefault();
+        //dialog.close();
+        var event = document.createEvent('Event');
+        event.initEvent('myEvent', true, true);
+        dialog.dispatchEvent(event);
+    });
+
+    try {
+        //dialog.showModal()
+        dialog.style.top = '50px';
+        dialog.style.display = 'block';
+        document.getElementsByTagName('body')[0].appendChild(dialog);
+    }
+    catch (err) {
+        alert(err);
+    }
+
+    //if using yield
+    if (caller.indexOf('yield') >= 0) {
+        return new Promise(function (resolve, reject) {
+            dialog.addEventListener('myEvent', function () {
+                var returnValue = document.getElementById('dialog-body').contentWindow.returnValue;
+                document.body.removeChild(dialog);
+                resolve(returnValue);
+            });
+        });
+    }
+
+        //if using eval
+        var isNext = false;
+        var nextStmts = caller.split('\n').filter(function (stmt) {
+            if (isNext || stmt.indexOf('showModalDialog(') >= 0)
+                return isNext = true;
+            return false;
+        });
+        dialog.addEventListener('close', function () {
+            var returnValue = document.getElementById('dialog-body').contentWindow.returnValue;
+            document.body.removeChild(dialog);
+            nextStmts[0] = nextStmts[0].replace(/(window\.)?showModalDialog\(.*\)/g, JSON.stringify(returnValue));
+            eval('{\n' + nextStmts.join('\n'));
+        });
+    throw 'Execution stopped until showModalDialog is closed';
+};
+})();
