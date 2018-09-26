7d34c2ed429288eab4eb54ae90b8c8e0d35c3929
Giuseppe De Marco <giuseppe.demarco@unical.it>
2018-09-26 14:38:33 +0200

    Create GeoIP.sh

---
+++ nginx/GeoIP.sh
@@ -0,0 +1,26 @@
+````
+# check nginx have geoip support
+nginx -v | grep nginx
+
+# this should be scheduled once per week:
+wget -O - http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz | gunzip -c > /opt/GeoIPdb/GeoIP.dat
+# otherwise per City: http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
+
+
+# then add this to your nginx configuration (default or site)
+    # geoip filter
+    geoip_country /opt/GeoIPdb/GeoIP.dat;
+    map $geoip_country_code $allowed_country {
+        #default no;
+        IT yes;
+    }
+
+    if ($allowed_country = no) {
+        return 444;
+    }
+    # end geoip
+
+
+# then reload service
+/etc/init.d/nginx reload
+
