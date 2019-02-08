ae5ace6e1ab3619e3f5584f58d098a085cb52f11
Giuseppe De Marco <giuseppe.demarco@unical.it>
2019-02-08 16:25:50 +0100

    Update caesar_cipher.py

--- OpenSSL/caesar_cipher.py
+++ OpenSSL/caesar_cipher.py
@@ -16,18 +16,20 @@ def ord_map(letters = string.printable, reverse=False):
     return ord_chars
 
 
-def get_shifted(offset, char_map=ord_map(), reverse=False):
+def get_shifted(offset, chars, reverse=False):
     """
     offset can be negative or positive integer
     char_map is ascii mapped to numbers by default
     """
     assert isinstance(offset, int)
+    char_map = ord_map(chars)
     len_char_map = len(char_map)
-    bound = int((len_char_map) * 1.38)
     char_n_list = sorted(list(char_map.values()))
 
+    bound = int(len_char_map)
     if abs(offset) > bound:
-        raise ValueError('offset must be lower than {}'.format(bound))
+        # raise ValueError('offset must be lower than {}'.format(bound))
+        offset = offset - int(len_char_map)
     shifted_ord_chars = dict()
     for char,number in char_map.items():
         shifted_pos = char_n_list.index(number) + offset
@@ -44,14 +46,18 @@ def get_shifted(offset, char_map=ord_map(), reverse=False):
     return shifted_ord_chars
 
 
-def crypt(text, secret):
-    enc_map = get_shifted(secret)
-    l = [ord_map(reverse=1)[p] for p in [enc_map[i] for i in text]]
+def crypt(text, secret, chars):
+    enc_map = get_shifted(secret, ord_map(chars))
+    try:
+        l = [ord_map(chars, reverse=1)[p] for p in [enc_map[i] for i in text]]
+    except Exception as e:
+        print(e)
+        raise ValueError('element not available in char map')
     return ''.join(l)
 
 
-def decrypt(text, secret):
-    enc_map = get_shifted(secret, reverse=1)
+def decrypt(text, secret, chars):
+    enc_map = get_shifted(secret, chars, reverse=1)
     l = [enc_map[ord(i)] for i in text]
     return ''.join(l)
 
