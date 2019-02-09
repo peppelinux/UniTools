b5e87c3e7aea0e73da21702a6f88afa001f21c05
Giuseppe De Marco <giuseppe.demarco@unical.it>
2019-02-09 15:09:41 +0100

    Delete AES-CBC-encrypt_file.sh

--- OpenSSL/AES-CBC-encrypt_file.sh
+++
@@ -1,6 +0,0 @@
-
-export FNAME="file_name.txt"
-export SUFFIX=".aes"
-
-openssl enc -in $FNAME -aes-256-cbc -pass pass:yourpassword > $FNAME$SUFFIX
-openssl enc -in $FNAME$SUFFIX -d -aes-256-cbc -pass pass:yourpassword #> logfile.x.log
