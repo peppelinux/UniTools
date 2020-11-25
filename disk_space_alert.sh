b39d837880deac72605a77e57adf6b810678748f
Giuseppe De Marco <giuseppe.demarco@unical.it>
2020-11-25 15:27:34 +0100

    Update disk_space_alert.sh

--- disk_space_alert.sh
+++ disk_space_alert.sh
@@ -5,9 +5,17 @@
 CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')
 THRESHOLD=90
 EMAIL=blahblah@email.cu
+FREE_RAM=$(free -m | grep Mem | awk -F' ' {'print $4'})
+
 
 if [ "$CURRENT" -gt "$THRESHOLD" ] ; then
-    mail -s 'Disk Space Alert' $EMAIL << EOF
+    mail -s 'Alert: Disk Space' $EMAIL << EOF
 Your root partition remaining free space is critically low. Used: $CURRENT%
+EOF
+fi
+
+if [ $(expr $FREE_RAM / 1024) -le 1 ]; then 
+     mail -s 'Alert: Available RAM less then 1GB' $EMAIL << EOF
+Current available RAM: $FREE_RAM GB
 EOF
 fi
